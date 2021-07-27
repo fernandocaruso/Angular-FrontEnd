6abbaa3b7558acdfcff46258c984251a2a10bdf5
Fernando Caruso <luisfernandocaruso95@gmail.com>
2021-07-27 16:00:44 -0300

    Implementação de Rotas Entrar e Cadastrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'}, // página inicial: caminho vazio = aba entrar! - pathMatch = compatibilidade total.
+  {path: 'entrar', component: EntrarComponent}, // path da página entrar. Atentar-se a não colocar / antes do caminho.
+  {path: 'cadastrar', component: CadastrarComponent}, // path da página cadastrar. Atentar-se a não colocar / antes do caminho.
+
+  // basicamente: se a página der em branco (pois não temos página inicial), será redirecionada através do redirectTo para uma página.
+  // logo, a página Entrar acaba sendo a inicial. 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
